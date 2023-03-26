af2daa64ea31e813e9784398d1f74df9589783ec
Darksw20 <ricardog.navi20@gmail.com>
2023-03-26 14:17:31 -0600

    First commit: Basic definition of an API

---
+++ api/controllers/HealthController.js
@@ -0,0 +1,13 @@
+/**
+ * HealthController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  health: function (req, res) {
+    return res.ok({ status: 'ok' });
+  }
+
+};
