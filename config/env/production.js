eb721ba845e5707b0adf9c018508ea8a02334321
scout <scout.dks@gmail.com>
2023-04-15 16:22:51 -0600

    Added production env

--- config/env/production.js
+++ config/env/production.js
@@ -44,8 +44,8 @@ module.exports = {
      *                                                                          *
      ***************************************************************************/
     default: {
-      // adapter: 'sails-mysql',
-      // url: 'mysql://user:password@host:port/database',
+      adapter: require("sails-mysql"),
+      url: process.env.DB_CONNECTION,
       //--------------------------------------------------------------------------
       //  /\   To avoid checking it in to version control, you might opt to set
       //  ||   sensitive credentials like `url` using an environment variable.
