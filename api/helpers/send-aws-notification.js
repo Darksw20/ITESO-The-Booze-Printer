f4a164e404f5eeff828e86386e99a4bf35ad3fd7
Scout <ricardog.navi20@gmail.com>
2023-05-10 02:44:31 -0600

    added route for home

--- api/helpers/send-aws-notification.js
+++ api/helpers/send-aws-notification.js
@@ -35,10 +35,12 @@ module.exports = {
         clientId: inputs.channel,
         host: process.env.IOT_CORE_ENDPOINT,
         port: 8883,
-        keyPath: `${process.env.PWD}/AWS_secrets/private.pem.key`,
-        certPath: `${process.env.PWD}/AWS_secrets/certificate.pem.crt`,
-        caPath: `${process.env.PWD}/AWS_secrets/AmazonRootCA1.cer`,
+        keyPath: `${process.env.HOME}/AWS_secrets/private.pem.key`,
+        certPath: `${process.env.HOME}/AWS_secrets/certificate.pem.crt`,
+        caPath: `${process.env.HOME}/AWS_secrets/AmazonRootCA1.cer`,
       });
+
+      console.log(process.env.PWD);
 
       const orders = await Order.find({
         where: { status: "ready" },
